4129ac92b36fed87a80555ccc4a1fe13a5a3aeb2
wch-os <wch1224.s@gmail.com>
2023-08-22 15:52:46 +0900

    가로수.py

--- 백준/Silver/2485. 가로수/가로수.py
+++ 백준/Silver/2485. 가로수/가로수.py
@@ -2,13 +2,7 @@ import sys
 from math import gcd
 input = sys.stdin.readline
 
-def gcd_of_list(lst):
-    a = lst[0]
-    for b in lst[1:]:
-        a = gcd(a, b)
-    return a
-
-
+#심어져 있는 가로수 개수
 N = int(input())
 
 #가로수 위치 입력
@@ -16,16 +10,21 @@ location = []
 for _ in range(N):
     location.append(int(input()))
 
-#간격들 최대공약수 찾기
+#가로수 사이 간격 찾기
 gaps = []
-for i in range(len(location) - 1):
-    gap = location[i+1] - location[i]
-    gaps.append(gap)
+for i in range(N-1):
+    gaps.append(location[i+1]-location[i])
 
-minGap = gcd_of_list(gaps)
+#간격, 최대공약수 찾기
+gapGcd = gaps[0]
+for i in range(1, N-1):
+    gapGcd = gcd(gapGcd, gaps[i])
 
-#심어야 할 가로수 세기
-s = location[0]
-e = location[len(location)-1]
+#같은 간격이 되도록 심어야 하는 가로수의 최소 개수
+    #간격을 최대공약수로 나누면, 간격 사이에 존재해야 하는 나무 개수를 알 수 있다.
+    #이미 그 사이에 나무가 1개가 심어져 있으므로 -1
+result = 0
+for i in gaps:
+    result = result + i // gapGcd - 1
 
-print(int((e - s) / minGap) - len(location) + 1)
+print(result)
