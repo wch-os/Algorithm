85ec63c82eea4637c215252ae2cbd0853a569ab6
wch-os <wch1224.s@gmail.com>
2023-09-07 12:39:58 +0900

    [Silver V] Title: 뒤집기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -2,7 +2,6 @@ s = input()
 
 zeroCount = 0
 oneCount = 0
-sequence = True
 if s[0] == "0":
     zeroCount += 1
 else:
@@ -11,7 +10,6 @@ else:
 for i in range(len(s)-1):
     # 연속적이지 않으면, 그 다음 숫자 기준으로 카운트
     if s[i] != s[i+1]:
-        sequence = False
         if s[i+1] == "0":
             zeroCount+=1
         else:
