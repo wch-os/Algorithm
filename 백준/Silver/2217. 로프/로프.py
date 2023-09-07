8620ea0fb65666c2ac9d77a5911290a57ee83e50
wch-os <wch1224.s@gmail.com>
2023-09-07 14:01:23 +0900

    [Silver IV] Title: 로프, Time: 136 ms, Memory: 37572 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.py
+++ 백준/Silver/2217. 로프/로프.py
@@ -6,15 +6,16 @@ N = int(input()) # N개의 로프
 
 weight = []
 for _ in range(N):
-    heapq.heappush(weight, int(input()))
+    weight.append(int(input()))
+
+# 내림차순 정렬
+weight.sort(reverse=True)
 
 # 로프들을 이용해서 들어올릴 수 있는 물체의 최대 중량
 maxWeight = 0
 
-# 우선순위 큐가 비어있지 않으면
-# 해당 로프를 사용하지 않는 것이 이득인지, 사용하는 것이 이득인지 판단
-while weight:
-    maxWeight = max(maxWeight,heapq.heappop(weight)*N)
-    N-=1
+# n개 or n-1개
+for i in range(N):
+    maxWeight = max(maxWeight, weight[i] * (i+1))
 
 print(maxWeight)
