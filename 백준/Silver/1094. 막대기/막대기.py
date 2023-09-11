d4d73e31289092dca5b5bdb8d7811ef6e713dbe3
wch-os <wch1224.s@gmail.com>
2023-09-11 16:48:13 +0900

    [Silver V] Title: 막대기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.py
+++ 백준/Silver/1094. 막대기/막대기.py
@@ -1,28 +1,12 @@
 x = int(input())
 
-own = [64]
-count = 0
+# x에 포함된 '비트 1'의 개수
+result = 0
 
-while True:
-    # 1. 합이 x보다 크다.
-    if sum(own) > x:
-        # 1. 가지고 있는 막대 중 길이가 가장 짧은 것을 절반으로 자른다.
-        divide = own.pop() // 2
-        own.append(divide)
-        own.append(divide)
+# 막대의 길이는 64 = 2^6
+for i in range(7):
+    # 'x'와 비트 AND 연산을 통해, 'i'번째 비트가 1로 설정되어 있는지 확인
+    if x & (1<<i):
+        result += 1
 
-        # 가정(절반 중 하나를 버리고, 남아있는 막대의 길이의 합이 X보다 크거나 같으면)
-        ifpop = own.pop()
-        if sum(own) >= x:
-            # 그대로 버린다.
-            continue
-        # 가정이 빗나갔으니, 다시 append 한다.
-        else:
-            own.append(ifpop)
-
-    # 막대를 풀로 붙이는 행위
-    count += 1
-    if sum(own) == x:
-        break
-        
-print(count)
+print(result)
