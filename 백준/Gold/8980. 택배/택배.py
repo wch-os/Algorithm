c6b9b4db9a8409a82512b0f0959ac965784eec07
wch-os <wch1224.s@gmail.com>
2023-09-22 21:09:54 +0900

    [Gold I] Title: 택배, Time: 3012 ms, Memory: 32276 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8980. 택배/택배.py
+++ 백준/Gold/8980. 택배/택배.py
@@ -1,32 +1,39 @@
-# 목표 번호 1 : 15점
+# 참고 : https://ddiyeon.tistory.com/36
+# 참고 : https://hillier.tistory.com/105
 
-import heapq
 import sys
 sys.stdin.readline
 
 N, C = map(int, input().split()) # 마을의 수, 트럭의 용량
 M = int(input()) # 박스 정보의 개수
+infos = []
 
 lst = [] # 박스 정보 데이터
 for _ in range(M):
-    send, receive, capacity = map(int, input().split())
+    send, receive, box = map(int, input().split())
+    infos.append((send, receive, box))
 
-    # 용량 초과일 경우
-    if capacity >= C:
-        capacity = C
+# 빠른 도착지를 기준으로 오름차순 정렬한다.
+infos.sort(key = lambda x : x[1])
 
-    heapq.heappush(lst, (-capacity, send, receive))
+# 각 마을에서 실을 수 있는 트럭 용량
+vilCapa = [C] * N
+total = 0
 
+for s, r, box in infos:
+    _min = C
 
-# 보내는 마을번호는 모두 1이므로, 트럭에 최대한 가득 싣기
-sumC = 0
-while lst:
-    capacity, start, end = heapq.heappop(lst)
-    capacity = -capacity
-    sumC += capacity
+    # 각 마을에서 실을 수 있는 용량 계산하기
+    for i in range(s, r):
+        # 해당 범위에서 보낼 수 있는 박스 중, 최소에 맞춘다.
+        if _min > min(vilCapa[i], box):
+            _min = min(vilCapa[i], box)
 
-    if sumC >= C:
-        sumC = C
-        break
+    # 트럭에 실은만큼, 실을 수 있는 용량을 재계산한다.
+    for i in range(s, r):
+        vilCapa[i] -= _min
 
-print(sumC)
+    # 실었던 박스를 더한다.
+    total += _min
+
+print(total)
