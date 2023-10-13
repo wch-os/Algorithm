4c6c9a11a7140f378b54e0001719302dd8c2b844
wch-os <wch1224.s@gmail.com>
2023-10-14 03:40:11 +0900

    [Gold III] Title: 양팔저울, Time: 52 ms, Memory: 34396 KB -BaekjoonHub

--- 백준/Gold/2629. 양팔저울/양팔저울.py
+++ 백준/Gold/2629. 양팔저울/양팔저울.py
@@ -1,3 +1,6 @@
+# solve() 종료 조건을 1 차이로 다르게 설정함.
+  # 이 과정에서 indexError를 방지하기 위해 weight[c-1]로 설정
+
 import sys
 input = sys.stdin.readline
 
@@ -11,6 +14,12 @@ see = list(map(int, input().split())) # 구슬의 무게들
 dp = [[0] * (500*n1+1) for i in range(n1+1)]
 
 def solve(c, w):
+    # 모든 추를 사용할 경우
+    # 'c==n1' 경우를 포함시켜, dp[c][w]를 갱신하게 만든다.
+       # But, weight[c]가 index over가 나지 않도록 weight[c-1]로 수정하여 weight[-1]로도 탐색할 수 있게 한다.
+    if c > n1:
+        return
+    
     # 저장된 값이 있으면 return
     if dp[c][w] != 0:
         return
@@ -18,13 +27,8 @@ def solve(c, w):
     # 지금 w의 무게는 측정이 가능함을 표시
     dp[c][w] = 1
 
-    # 모든 추를 사용할 경우
-    # 해당 무게까지 '측정 가능'을 표시한 후, 모든 추를 사용하는지 여부를 판단해야 함!!
-    if c == n1:
-        return
-
-    solve(c+1, w + weight[c]) # 오른쪽 저울에 올린 경우
-    solve(c+1, abs(w - weight[c])) # 왼쪽 저울에 올린 경우
+    solve(c+1, w + weight[c-1]) # 오른쪽 저울에 올린 경우
+    solve(c+1, abs(w - weight[c-1])) # 왼쪽 저울에 올린 경우
     solve(c+1, w)  # 현재 인덱스의 추를 사용하지 않는 경우
 
 solve(0, 0)
