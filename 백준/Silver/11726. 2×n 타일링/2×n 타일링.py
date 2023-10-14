28f7ab3772e34300fd186f9a9ebe83301b30d65b
wch-os <wch1224.s@gmail.com>
2023-10-14 14:31:13 +0900

    [Silver III] Title: 2×n 타일링, Time: 44 ms, Memory: 31296 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,17 @@
+import sys
+
+sys.setrecursionlimit(10**9)
+
+def fibo(n):
+    if n == 0 or n == 1:
+        return 1
+    elif dp[n] != -1:
+        return dp[n]
+
+    dp[n] = (fibo(n-1) % 10007) + (fibo(n-2) % 10007)
+
+    return dp[n] % 10007
+
+N = int(input())
+dp = [-1] * (N+1)
+print(fibo(N))
