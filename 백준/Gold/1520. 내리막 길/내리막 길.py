b8655774cc763148efb64c63a9edb5451dcd94be
wch-os <wch1224.s@gmail.com>
2023-10-16 00:45:04 +0900

    [Gold III] Title: 내리막 길, Time: 144 ms, Memory: 42780 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.py
+++ 백준/Gold/1520. 내리막 길/내리막 길.py
@@ -1,38 +1,63 @@
-# 참고 : https://velog.io/@nathan29849/BAEKJOON-1520-%EB%82%B4%EB%A6%AC%EB%A7%89-%EA%B8%B8-DFS-BFS-python
+# dfs 풀이 : 시작점 (0,0)에 경로 횟수가 저장됨
+# 이전 제출 코드의 문제점
+   # 1. dp 중복을 거르지 않았다.
+   # 2. dp 초기화를 0으로 했다.
+      # '0'으로 하면 시간초과가 난다.
+      # Why?
+        # 경로가 없을 경우에는 DP[][]에 0이 저장될 것이다.
+        # 이런 경우에 탐색을 하였어도, 0이 저장되었으므로 재탐색을 할 것이다.
 
-# bfs를 사용할 때 "deque이 아닌 PriortyQueue를 사용해야 하는 이유"
-   # deque을 쓰면, 돌아가는 경로에서 제대로 dp[][] 반영이 되지 않는다.
-   # 높이를 기준으로 한 최소힙을 써야 한다.
+# dfs에서의 dp 정의 : (x,y)에서 도착지점까지 갈 수 있는 경로의 개수
+# bfs에서의 dp 정의 : 출발지점에서 (x,y)까지 갈 수 있는 경로의 개수
 
-import heapq
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10**9)
 
-a, b = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(a)]
-dp = [[0] * b for _ in range(a)]
-dp[0][0] = 1
+def dfs(x, y):
+    if x == a-1 and y == b-1:
+        return 1
 
-q = []
-heapq.heappush(q, [-arr[0][0] ,0, 0])
+    if dp[x][y] != -1:
+        return dp[x][y]
 
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
-
-while q:
-    trash ,x, y = heapq.heappop(q)
-
+    dp[x][y] = 0 # 탐색했다는 표시
+    
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
 
         # 낮은 높이로 이동이 가능하면, queue에 넣음.
         if 0 <= nx < a and 0 <= ny < b and arr[x][y] > arr[nx][ny]:
-            # 첫 방문일 경우에만 heap에 넣음.
-              # 탐색시간 줄이기 위해 '이전 경로 수를 더해주는 방식'으로 접근한다.
-            if dp[nx][ny] == 0:
-                heapq.heappush(q, [-arr[nx][ny], nx, ny])
+            dp[x][y] += dfs(nx, ny)
 
-            dp[nx][ny] += dp[x][y] # 이전 경로 개수를 더해준다.
+    return dp[x][y]
 
-print(dp[a-1][b-1])
+
+
+a, b = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(a)]
+dp = [[-1] * b for _ in range(a)]
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+print(dfs(0, 0))
+
+# q = []
+# heapq.heappush(q, [-arr[0][0] ,0, 0])
+# while q:
+#     trash ,x, y = heapq.heappop(q)
+#
+#     for i in range(4):
+#         nx = x + dx[i]
+#         ny = y + dy[i]
+#
+#         # 낮은 높이로 이동이 가능하면, queue에 넣음.
+#         if 0 <= nx < a and 0 <= ny < b and arr[x][y] > arr[nx][ny]:
+#             # 첫 방문일 경우에만 heap에 넣음.
+#               # 탐색시간 줄이기 위해 '이전 경로 수를 더해주는 방식'으로 접근한다.
+#             if dp[nx][ny] == 0:
+#                 heapq.heappush(q, [-arr[nx][ny], nx, ny])
+#
+#             dp[nx][ny] += dp[x][y] # 이전 경로 개수를 더해준다.
