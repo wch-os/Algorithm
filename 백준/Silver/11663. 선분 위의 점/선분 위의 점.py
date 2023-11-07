03c5e9527b3e293d81933a708b74d719b0016f8f
wch-os <wch1224.s@gmail.com>
2023-11-08 02:40:11 +0900

    [Silver III] Title: 선분 위의 점, Time: 384 ms, Memory: 131296 KB -BaekjoonHub

--- 백준/Silver/11663. 선분 위의 점/선분 위의 점.py
+++ 백준/Silver/11663. 선분 위의 점/선분 위의 점.py
@@ -1,6 +1,14 @@
+# 풀이 시간 : 1시간
+# 시간복잡도 : O(2MlogN)
+# 공간복잡도 : O(N or M)
+# 참고 : -
+
 # 1. 점들을 먼저 오름차순 정렬한다.
 # 2. 각 선분의 시작점(upper)과 끝점(lower)의 index를 찾는다
 # 3. 해당 index 범위 내의 dot 개수를 구한다.
+
+# lower, upper bound 문제는 빠르게 이해함.
+# 그러나 하나의 함수로 통일하고, 후처리 하는 과정에서 시간을 소모했다.. (결국 따로 메소드 만듦)
 
 import sys
 input = sys.stdin.readline
@@ -49,7 +57,7 @@ for s, e in line:
     # 선분의 시작점을 기준으로, lower_bound
         # 즉, "s 이상의 최소 인덱스"를 찾는다.
     indexS = lower_search(s)
-    
+
     # 선분의 끝점을 기준으로, upper_bound
         # 즉, "e 초과의 최소 인덱스"를 찾는다.
     indexE = upper_search(e)
