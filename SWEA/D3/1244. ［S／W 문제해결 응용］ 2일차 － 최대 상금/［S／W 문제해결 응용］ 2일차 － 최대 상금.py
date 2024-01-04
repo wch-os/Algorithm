caaf9cea98737d4b6b2cc0f1de5fa4c3963b77c3
wch-os <wch1224.s@gmail.com>
2024-01-04 21:52:05 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 263 ms, Memory: 63,860 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
@@ -1,3 +1,28 @@
+# 풀이 시간 : 1시간 30분 + 1시간
+# 시간복잡도 : O(15^10 - 중복탐색)
+# 공간복잡도 : O(n)
+# 참고 : https://www.youtube.com/watch?v=vGU8h1rneGw
+
+# 입력 3번을 보면 dp로 풀 수 없음.
+    # 이전 dp[i-1], dp[i-2]를 활용한 규칙성을 찾을 수 없다.
+
+# 1번째 자릿수를, 제일 큰 수를 찾아바꾸기
+# 그리디하게 풀려고 했지만 방법을 찾지 못해 포기
+
+# 풀이
+# 기본적으로 dfs로 완전 탐색을 한다는 생각으로 접근을 하고 백트래킹 하여,
+# (k : 남은 교환횟수, num : 현재 만들어진 숫자)의 중복된 탐색을 하지 않도록 하여 풀어야 했다.
+
+def check(depth):
+    # 이전에 탐색한 노드면 제외한다.
+    n = int("".join(map(str, numbers))) * 10 + depth  # 튜플이 아닌, 일반 정수 교유값으로 최적화
+    if n in visited:
+        return False
+
+    # 처음 가 본 노드라면 계속 탐색을 진행한다.
+    visited.append(n)
+    return True
+
 def dfs(depth):
     global result
 
@@ -10,11 +35,8 @@ def dfs(depth):
             # 백트래킹 완전 탐색
             numbers[i], numbers[j] = numbers[j], numbers[i]
 
-            # 이전에 탐색한 노드면 제외한다.
-            check = int("".join(map(str, numbers))) * 10 + depth # 튜플이 아닌, 일반 정수 교유값으로 최적화
-            if check not in visited:
+            if check(depth):
                 dfs(depth+1)
-                visited.append(check)
 
             numbers[i], numbers[j] = numbers[j], numbers[i]
 
