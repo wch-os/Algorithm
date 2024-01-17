0e0b9c00876a204861ebb433c64f6bdbaa33b234
wch-os <wch1224.s@gmail.com>
2024-01-17 15:24:27 +0900

    [Gold IV] Title: 목장 건설하기, Time: 1092 ms, Memory: 47688 KB -BaekjoonHub

--- 백준/Gold/14925. 목장 건설하기/목장 건설하기.py
+++ 백준/Gold/14925. 목장 건설하기/목장 건설하기.py
@@ -1,30 +1,17 @@
-# farm[M-1][N-1]에 장애물이 있는 경우, dp[M][N] 값에 결과값이 들어있지 않다.
-# 아.. 모두 장애물일 경우
-    # result 초기값이 그대로 출력이 되므로
-    # result 초기값을 0으로 설정해 주어야 한다.
-
-# 풀이 시간 : 1시간 5분 + 40분
-# 시간복잡도 : O(MN)
-# 공간복잡도 : O(MN)
-# 참고 : https://www.acmicpc.net/source/66884805
-#       https://sangdo913.tistory.com/116
+# 내 코드와 뭐가 틀린거지?
 
 import sys
 input = sys.stdin.readline
 
-M, N = map(int, input().split())
-farm = [list(map(int, input().split())) for _ in range(M)]
+m,n = map(int, input().split())
+matrix = [list(map(int, input() .split())) for _ in range(m)]
+dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+maxFarm = 0
 
-# dp[i][j] : i, j 에서 지을 수 있는 가장 큰 정사각형 목장 크기
-# dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
-    # 왼쪽, 위쪽, 왼쪽 위 대각선
-dp = [[0] * (N+1) for _ in range(M+1)]
+for i in range(1,m+1):
+  for j in range(1,n+1):
+    if not matrix[i-1][j-1]:
+      dp[i][j] = min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1]) + 1
+      maxFarm = max(maxFarm, dp[i][j])
 
-result = 0
-for i in range(1, M+1):
-    for j in range(1, N+1):
-        if farm[i-1][j-1] == 0:
-            dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
-            result = max(result, dp[i][j])
-
-print(result)
+print(maxFarm)
