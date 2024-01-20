b54b4181ab6c36a7f90a8e885e4c1ae5445ae847
wch-os <wch1224.s@gmail.com>
2024-01-21 05:56:03 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.py
@@ -0,0 +1,34 @@
+# 풀이 시간 : 10 + 25분
+# 시간복잡도 : O(N)
+# 공간복잡도 : O(1)
+# 참고 : -
+
+# 1 2 6 24 120 720(6!) 5040(7!)
+# 0이 아닌 숫자가 나올 때까지의 수에서 팩토리얼을 계속 계산하자
+
+# 첫번째 제출 코드
+    # 1의 자리만 남는 코드가 아니다.
+    # 결국 fact가 INF 값이 되고 만다.
+
+    # 1의 자리만 남게 하면 된다.
+
+N = int(input())
+
+fact = 1
+zeroCount = 0
+for i in range(1, N+1):
+    fact *= i
+
+    while True:
+        if fact % 10 == 0: # 팩토리얼 값의 뒷자리가 0이 있으면 카운트
+            while True: # 1의 자리만 남기도록 한다.
+                if fact % 10 != 0:
+                    fact %= 10
+                    break
+                fact /= 10
+                zeroCount += 1
+
+        else:
+            break
+
+print(zeroCount)
