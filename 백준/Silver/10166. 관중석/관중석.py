40171bced5b9402d2e721f4ecf647e76e3b1482c
wch-os <wch1224.s@gmail.com>
2024-02-20 17:51:29 +0900

    [Silver I] Title: 관중석, Time: 652 ms, Memory: 63908 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10166. 관중석/관중석.py
+++ 백준/Silver/10166. 관중석/관중석.py
@@ -1,15 +1,25 @@
-a, b = map(int, input().split())
+# 정답 : https://dreamtreeits.tistory.com/121
 
-# 의자 각도 저장
-circle = set()
+from sys import stdin
+from math import gcd
 
-for i in range(a, b+1):
-    init = 360 / i
-    k = init
 
-    if init not in circle:
-        while init <= 360:
-            circle.add(init)
-            init += k
+def solve():
+    a, b = map(int, stdin.readline().split())
+    arr = [[0] * b for _ in range(b)]
 
-print(len(circle))
+    ans = 0
+
+    for i in range(a, b + 1):
+        for j in range(1, i + 1):
+            g = gcd(i, j)
+
+            x, y = i // g, j // g
+
+            if not arr[x-1][y-1]:
+                arr[x-1][y-1] = 1
+                ans += 1
+
+    print(ans)
+
+solve()
