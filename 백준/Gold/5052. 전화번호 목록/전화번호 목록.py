96737f5822343836b8dcce2295fdc5ce53e32c8c
wch-os <wch1224.s@gmail.com>
2024-03-10 20:13:26 +0900

    [Gold IV] Title: 전화번호 목록, Time: 976 ms, Memory: 47500 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -1,3 +1,10 @@
+# 이전 코드 : 트리에 모든 num을 insert() 하고, 길이가 짧은 번호부터 일관성 체크를 진행한다.
+# 현재 코드 : 길이가 짧은 번호부터, 일관성 체크를 하고 insert() 한다.
+
+# 참고 : https://alpyrithm.tistory.com/72
+# https://m.blog.naver.com/cjsencks/221740232900
+# https://velog.io/@cjkangme/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%ED%8A%B8%EB%9D%BC%EC%9D%B4Trie-%EC%9E%90%EB%A3%8C%EA%B5%AC%EC%A1%B0-%ED%8C%8C%EC%9D%B4%EC%8D%AC
+
 import sys
 input = sys.stdin.readline
 
@@ -34,12 +41,12 @@ class Trie:
         for s in string:
             if s in curr_node.children:
                 curr_node = curr_node.children[s]
-            else:
-                return False
+            else: # 존재하지 않는 s이면 break 이후, 리프노드인지 확인한다.
+                break
 
-        # children이 남아있으면 전화번호가 겹치는 것을 의미한다.
+        # curr_node가 리프노드인 것은 일관성이 없는 번호임을 의미한다.
         # 911, 9112 → NO
-        if curr_node.children:
+        if curr_node.isLeaf:
             return False
 
         # 남아있지 않으면 일관성 있는 독립적인 번호이다.
@@ -50,7 +57,7 @@ class Trie:
 
 
 T = int(input().strip())
-for _ in range(T):
+for t in range(T):
     N = int(input())
     nums = []
     trie = Trie()
@@ -59,15 +66,17 @@ for _ in range(T):
     for _ in range(N):
         num = input().strip()
         nums.append(num)
-        trie.insert(num)
 
     # 길이가 짧은 전화번호부터 search를 진행한다.
         # 일관성 체크를 더 빠르게 하기 위함.
     nums.sort()
     for num in nums:
+        # 일관성 없는 번호일 경우 break
         if not trie.search(num):
             flag = False
             break
+
+        trie.insert(num)
 
     if flag:
         print("YES")
