4ad8955d6ea10745e56d2b7efcd39b622ba56ea1
wch-os <wch1224.s@gmail.com>
2024-03-14 23:17:32 +0900

    [Gold III] Title: 드래곤 커브, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/15685. 드래곤 커브/드래곤 커브.py
+++ 백준/Gold/15685. 드래곤 커브/드래곤 커브.py
@@ -11,37 +11,44 @@
 # 추가로 생각할 것
     # 문제 : 네 꼭짓점이 모두 드래곤 커브의 일부인 경우
     # 간선으로 둘러싼 정사각형 개수를 출력하라고 하면?
+    
 
 import sys
 input = sys.stdin.readline
 
-# → ↑ ← ↓
-# arrow = [(0, 1), (-1, 0), (0, -1), (1, 0)]
-arrowX = [0, -1, 0, 1]
-arrowY = [1, 0, -1, 0]
-visited = [[0 for _ in range(101)] for _ in range(101)]
+visited = [[0 for _ in range(102)] for _ in range(102)]
 
-def move(x, y, stack):
-    for i in stack:
+def main():
+    N = int(input())  # 커브 개수
+
+    for _ in range(N):
+        # (x, y) : 커브 시작점 / d : 시작 방향 / g : 세대
+        y, x, d, g = map(int, input().split())
+        visited[x][y] = 1
+
+        curve = [d]  # 방향에 맞게끔 arrowX, Y 설정
+        for _ in range(g):
+            for j in range(len(curve) - 1, -1, -1):
+                curve.append((curve[j] + 1) % 4)  # move[j] = d
+
+        move(x, y, curve)
+
+
+def move(x, y, curve):
+    # → ↑ ← ↓
+    arrowX = [0, -1, 0, 1]
+    arrowY = [1, 0, -1, 0]
+
+    for i in curve:
         x += arrowX[i]
         y += arrowY[i]
 
         visited[x][y] = 1
 
-    # if not stack:
-    #     return
-    #
-    # i = stack.pop()
-    #
-    # nx = x + arrowX[i]
-    # ny = y + arrowY[i]
-    #
-    # visited[nx][ny] = 1
-    # move(nx, ny, stack)
-
 
 def check():
     squareCnt = 0
+
     for i in range(100):
         for j in range(100):
             if visited[i][j] and visited[i][j+1] and visited[i+1][j] and visited[i+1][j+1]:
@@ -49,27 +56,7 @@ def check():
 
     return squareCnt
 
-def main():
-    N = int(input()) # 커브 개수
 
-    for _ in range(N):
-        # (x, y) : 커브 시작점 / d : 시작 방향 / g : 세대
-        y, x, d, g = map(int, input().split())
-        visited[x][y] = 1
-
-        stack = [d]
-        turnStack = [d]
-        for _ in range(g):
-            # 현재 그려져 있는 그림을 90도로 회전시키고, 기존 그림과 합친다
-            while turnStack:
-                idx = turnStack.pop() # 기존 그림 회전
-                idx = (idx + 1) % 4
-                stack.append(idx) # 기존 그림과 합친다.
-
-            # 현재 그림을, n세대까지 다시 회전시키기 위함
-            turnStack = stack.copy()
-
-        move(x, y, stack)
 
 if __name__ == "__main__":
     main()
