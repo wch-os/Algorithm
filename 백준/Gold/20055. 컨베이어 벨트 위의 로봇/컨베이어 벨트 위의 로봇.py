41f1ec3bdeddcd1d99a970a76a937c8291f59d9a
wch-os <wch1224.s@gmail.com>
2024-04-07 22:26:47 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 4976 ms, Memory: 34088 KB -BaekjoonHub

---
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -0,0 +1,63 @@
+from collections import deque
+
+N, K = map(int, input().split())
+remain = list(map(int, input().split()))
+judge = [False for _ in range(2*N)]
+
+# 0번째 위치 : 올리는 위치
+# N-1번째 위치 : 내리는 위치
+upCursor = 0 # 올리는 위치
+downCursor = N-1 # 내리는 위치
+
+depth = 0
+zeroCount = 0
+robot = deque()
+while True:
+    if zeroCount >= K:
+        break
+
+    upCursor -= 1 # 1바퀴 회전
+    downCursor -= 1
+
+    if upCursor == -2*N:
+        upCursor = 0
+        downCursor = N-1
+
+    # 이전에 배치된 로봇들이 이동 가능한 지 파악
+    for _ in range(len(robot)):
+        r = robot.popleft()
+        judge[r] = False # 이동하기 위해, 일단 현재 위치 로봇 제거
+
+        # 내리는 위치일 경우
+        if r == downCursor or r == downCursor - (2*N):
+            continue
+
+        # 내리는 위치가 아닐 경우
+        else:
+            if not judge[r+1] and remain[r+1]: # 다음 위치에 로봇이 없고, 다음 위치가 내구도가 있는 경우
+                # 이동하려는 위치에 로봇이 없다.
+                remain[r + 1] -= 1  # 내구도 감소
+                if remain[r + 1] == 0:
+                    zeroCount += 1
+                if r + 1 == downCursor or r + 1 == downCursor - (2*N):
+                    continue
+                robot.append(r+1) # 새로운 위치에 로봇 배치
+                judge[r+1] = True
+
+            else: # 내구도가 없는 경우
+                robot.append(r)
+                judge[r] = True
+
+
+
+    # 올리는 위치에 있는 칸의 내구도가 0이 아니면 올리는 위치에 로봇을 올린다.
+    if remain[upCursor]:
+        remain[upCursor] -= 1
+        if remain[upCursor] == 0:
+            zeroCount += 1
+        robot.append(upCursor)
+        judge[upCursor] = True
+
+    depth += 1
+
+print(depth)
