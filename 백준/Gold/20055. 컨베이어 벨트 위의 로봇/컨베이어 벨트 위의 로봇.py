ae0d7deeffdc2b8f20f44d0e5ae6e28e2127822e
wch-os <wch1224.s@gmail.com>
2024-04-07 23:04:49 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 2488 ms, Memory: 34036 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -1,60 +1,37 @@
 from collections import deque
 
 N, K = map(int, input().split())
-remain = list(map(int, input().split()))
-judge = [False for _ in range(2*N)]
+belt = deque(map(int, input().split()))
+robot = deque(False for _ in range(N))
 
-upCursor = 0 # 올리는 위치
-downCursor = N-1 # 내리는 위치
-
-depth = 0 # 단계
-zeroCount = 0 # 내구도가 0인 칸의 개수
-robot = deque() 
-
+depth = 0
 while True:
-    if zeroCount >= K:
+    if belt.count(0) >= K:
+        print(depth)
         break
-    # 1바퀴 회전
-    upCursor -= 1
-    downCursor -= 1
 
-    if upCursor == -2*N:
-        upCursor = 0
-        downCursor = N-1
+    # 1. 컨베이어 벨트와 로봇을 1칸 회전한다.
+    belt.rotate(1)
+    robot.rotate(1)
 
-    # 1. 이전에 배치된 로봇들이 이동 가능한 지 파악
-    for _ in range(len(robot)):
-        r = robot.popleft()
-        judge[r] = False # 이동하기 위해, 일단 현재 위치 로봇 제거
+    # 로봇이 내리는 위치는 바로 내리도록 한다.
+    robot[N - 1] = False
 
-        # 내리는 위치일 경우
-        if r == downCursor or r == downCursor - (2*N):
-            continue
+    # 2. 가장 먼저 벨트에 올라간 로봇부터, 한 칸씩 옆으로 이동하게 한다.
+    # (N-2, N-1) ~ (-1, 0) 까지 밀어준다. # 테트리스와 비슷
+    for i in range(N - 2, -1, -1):
+        if robot[i]:
+            # 다음 칸 내구도 & 로봇 유무 판단
+            if belt[i+1] >= 1 and not robot[i+1]:
+                robot[i+1], robot[i] = True, False
+                belt[i+1] -= 1
 
-        # 내리는 위치가 아닐 경우
-        else:
-            if not judge[r+1] and remain[r+1]: # 다음 위치에 로봇이 없고, 다음 위치가 내구도가 있는 경우
-                remain[r + 1] -= 1  # 내구도 감소
-                if not remain[r + 1]: 
-                    zeroCount += 1
-                if r + 1 == downCursor or r + 1 == downCursor - (2*N):
-                    continue
-                robot.append(r+1) # 새로운 위치에 로봇 배치
-                judge[r+1] = True
+    # 옆으로 이동했으므로, 내리는 위치는 다시 판단해준다.
+    robot[N - 1] = False
 
-            else: # 다음 위치에 로봇이 있거나, 내구도가 없는 경우
-                robot.append(r)
-                judge[r] = True
+    # 3. 올리는 칸에 로봇을 올린다.
+    if belt[0]:
+        belt[0] -= 1
+        robot[0] = True
 
-    # 2. 올리는 위치에 있는 칸의 내구도가 0이 아니면, 로봇을 올린다.
-    if remain[upCursor]:
-        remain[upCursor] -= 1
-        if remain[upCursor] == 0:
-            zeroCount += 1
-        robot.append(upCursor)
-        judge[upCursor] = True
-
-    # 3. 해당 단계를 마무리 한다.
     depth += 1
-
-print(depth)
