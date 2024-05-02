b22fe55fe2ef85800162712fd7ba9dc5261da182
wch-os <wch1224.s@gmail.com>
2024-05-02 16:38:29 +0900

    [Gold III] Title: 마법사 상어와 토네이도, Time: 1120 ms, Memory: 43568 KB -BaekjoonHub

--- 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.py
+++ 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.py
@@ -1,102 +1,69 @@
-# 풀이 시간 : 1시간 50분
-# 시간복잡도 : O(N^2)
-# 공간복잡도 : O(N^2)
-# 참고 : -
+# https://dev-cheddung.tistory.com/103
 
-# 범위를 90도 회전할 때 대칭이동으로 생각해서 풀었다.
-# 지금까지 90도 회전문제가 나올 때마다 규칙을 생각해서 풀었는데 대칭 이동으로 외워두고 풀면 빠르게 풀릴 것 같다.
-# +. 문제에서 a는 55%가 아닌 "이동하지 못한 모래의 양"으로 소수점 버림을 고려해야 한다.
+import math
 
-# 서(x, y)
-# 남(-y, -x)
-# 동(-x, -y)
-# 북(y, x)
+N = 0  # 격자 크기
+sand = 0  # 모래의 양
+cDir = 0  # 현재 방향(좌측부터 시작)
+num = 1  # 현재 이동 크기
+A = []  # 격자
+cR, cC = 0, 0  # 현재 위치
+dir = [(0, -1), (1, 0), (0, 1), (-1, 0)]  # 좌, 하, 우, 상
 
-import sys
-input = sys.stdin.readline
+# 확산 방향과 비율
+diff = [
+    [(-1, 1, 1), (1, 1, 1), (-2, 0, 2), (2, 0, 2), (0, -2, 5), (-1, 0, 7), (1, 0, 7), (-1, -1, 10), (1, -1, 10)],  # 좌
+    [(-1, -1, 1), (-1, 1, 1), (0, -2, 2), (0, 2, 2), (2, 0, 5), (0, -1, 7), (0, 1, 7), (1, -1, 10), (1, 1, 10)],  # 하
+    [(-1, -1, 1), (1, -1, 1), (-2, 0, 2), (2, 0, 2), (0, 2, 5), (-1, 0, 7), (1, 0, 7), (-1, 1, 10), (1, 1, 10)],  # 우
+    [(1, -1, 1), (1, 1, 1), (0, -2, 2), (0, 2, 2), (-2, 0, 5), (0, -1, 7), (0, 1, 7), (-1, -1, 10), (-1, 1, 10)],  # 상
+]
 
-def rangeCheck(x, y):
-    if 0 <= x < N and 0 <= y < N:
-        return True
 
-    return False
+def move():
+    global sand, cR, cC
+    cR, cC = cR + dir[cDir][0], cC + dir[cDir][1]
+    remain = A[cR][cC]
 
-# O(20)
-def windSand(x, y, boardY, wind):
-    global result
-
-    sand = [  # x 좌표 중심
-        [-2, -1, 2],
-        [-1, -2, 10], [-1, -1, 7], [-1, 0, 1],
-        [0, -3, 5],
-        [1, -2, 10], [1, -1, 7], [1, 0, 1],
-        [2, -1, 2],
-        [0, -2, 0], # 나머지
-    ]
-
-    if wind == 1: # ↓
-        for i in range(10):
-            sand[i][0], sand[i][1] = -sand[i][1], -sand[i][0] # '←' y = -x 대칭
-    elif wind == 2: # →
-        for i in range(10):
-            sand[i][0], sand[i][1] = -sand[i][0], -sand[i][1] # 원점 대칭
-    elif wind == 3: # ↑
-        for i in range(10):
-            sand[i][0], sand[i][1] = sand[i][1], sand[i][0] # y = x 대칭
-
-    sumPlusSand = 0
     for i in range(9):
-        # (nx, ny) : 바람에 의해 모래가 이동할 장소
-        nx = x + sand[i][0]
-        ny = y + sand[i][1]
+        nR, nC, percent = cR + diff[cDir][i][0], cC + diff[cDir][i][1], diff[cDir][i][2]
+        value = math.trunc(A[cR][cC] * percent / 100)
+        if nR < 0 or nR >= N or nC < 0 or nC >= N:
+            sand += value
+        else:
+            A[nR][nC] += value
+        remain -= value
 
-        plusSand = int(boardY * (sand[i][2] / 100))
-        sumPlusSand += plusSand # 이동하는 모래의 양 (소수점 아래는 버린다..!!)
-        if rangeCheck(nx, ny):
-            board[nx][ny] += plusSand
-        else: # 범위 바깥
-            result += plusSand
-
-    nx = x + sand[9][0]
-    ny = y + sand[9][1]
-    if rangeCheck(nx, ny):
-        board[nx][ny] += (boardY - sumPlusSand) # 이동하지 않는 모래의 양
+    aR, aC = cR + dir[cDir][0], cC + dir[cDir][1]
+    if aR < 0 or aR >= N or aC < 0 or aC >= N:
+        sand += remain
     else:
-        result += (boardY - sumPlusSand)
-
-#O(N^2)
-def move(x, y):
-    # ← ↓ → ↑
-    direction = [[0, -1], [1, 0], [0, 1], [-1, 0]]
-
-    cnt = 0  # 방향 횟수
-
-    while True:
-        for i in range(4):
-            # ← ↓ →→ ↑↑ ←←← ↓↓↓
-            if i == 0 or i == 2:
-                cnt += 1
-
-            for _ in range(cnt):
-                if x == 0 and y == 0:
-                    return
-
-                # (x, y) : x 좌표
-                # (nx, ny) : y 좌표
-                nx = x + direction[i][0]
-                ny = y + direction[i][1]
-
-                # 바람에 의해 모래가 날린다.
-                windSand(x, y, board[nx][ny], i)
-
-                x, y = nx, ny
+        A[aR][aC] += remain
+    A[cR][cC] = 0
 
 
-N = int(input())
-board = [list(map(int, input().split())) for _ in range(N)]
+def tornado():
+    global num, cDir, cR, cC
+    cR, cC = N // 2, N // 2
 
-result = 0
-c = N // 2
-move(c, c)
+    while num < N:
+        for _ in range(2):
+            for _ in range(num):
+                move()
+            cDir = (cDir + 1) % 4
+        num += 1
 
-print(result)
+    num -= 1
+    for _ in range(num):
+        move()
+
+
+def main():
+    global N, A
+    N = int(input())
+    A = [[int(x) for x in input().split()] for _ in range(N)]
+    tornado()
+    print(sand)
+
+
+if __name__ == "__main__":
+    main()
