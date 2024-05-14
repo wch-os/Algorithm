0aa92f98068b3651cd95118a7f1769cc0c5f1cb9
문선호 <wch1224.s@gmail.com>
2024-05-14 19:03:57 +0900

    [Silver IV] Title: 정수 제곱근, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2417. 정수 제곱근/정수 제곱근.py
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.py
@@ -1,27 +1,19 @@
-# 풀이 시간 : 10분
-# 시간복잡도 : O(logn)
-# 공간복잡도 : -
-# 참고 : 개념 정리
-    # 등호 판단 : 1) https://blossoming-man.tistory.com/entry/%EC%9D%B4%EC%A7%84-%ED%83%90%EC%83%89-Binary-Search-%EA%B2%BD%EA%B3%84-%EC%84%A4%EC%A0%95%EC%9D%84-%EC%96%B4%EB%96%BB%EA%B2%8C-%ED%95%B4%EC%95%BC-%ED%95%98%EB%8A%94%EA%B0%80
-    #           2) https://eine.tistory.com/entry/%EC%9D%B4%EC%A7%84-%ED%83%90%EC%83%89-%EC%9D%B4%EB%B6%84-%ED%83%90%EC%83%89binary-search-%EA%B5%AC%ED%98%84%EC%8B%9C-%EA%B3%A0%EB%A0%A4%ED%95%A0-%EA%B2%83%EB%93%A4
-    # Lower, Upper : https://yoongrammer.tistory.com/105
+# lowerbound 문제
+# n 이상인 lowerbound를 찾으면 된다.
 
-def search(start, end):
-    while start < end:
-        mid = (start + end) // 2
+def lowerBound():
+    lo, hi = -1, num + 1
 
-        if mid * mid < n:
-            start = mid + 1
+    while lo + 1 < hi:
+        mid = (lo + hi) // 2
 
+        if mid ** 2 < num:
+            lo = mid
         else:
-            end = mid
+            hi = mid
 
-    return end
+    return hi
 
 
-n = int(input())
-
-s = 0
-e = n
-
-print(search(s, e))
+num = int(input())
+print(lowerBound())
