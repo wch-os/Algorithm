f3a8750c2f1fa05be3e87c4740c30d9c6be2deba
문선호 <wch1224.s@gmail.com>
2024-05-15 19:37:00 +0900

    [Gold IV] Title: 드래곤 앤 던전, Time: 260 ms, Memory: 55660 KB -BaekjoonHub

--- 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.py
+++ 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.py
@@ -1,60 +1,19 @@
-# 풀이 시간 : 14분 + 15분
-# 시간복잡도 : O(N * log(N*10^12)
+# 풀이 시간 : 14분 + 5분
+# 시간복잡도 : O(N)
 # 공간복잡도 : O(N)
 # 참고 : -
 
-# 이분 탐색으로 접근해보자.
-# 문제에서 요구하는 것은 '던전을 돌파하기 위한 최소 hp'이다.
-# hp를 범위로 설정하고, 결정 문제를 'mid의 체력으로 던전을 통과할 수 있는가?'로 정한다.
+# 1번째 틀린 코드 수정
 
-# 유의할 점은 범위이다.
-# 유저의 공격력이 1, 몬스터의 체력이 10^6 일 때 10^6-1번 맞게 된다.
-# 그러한 방이 N개 있으며, 몬스터의 최대 공격력 또한 10^6이니
-# (방의 개수) * (몬스터의 최대 공격력) * (1개의 방에서 최대로 맞게 되는 경우의 수)
-# 무조건 통과할 수 있는 hp, hi를 N * 10^12 로 설정한다.
+# 각 방을 차례대로 돌파한다.
+    # t = 1, 방에서 몬스터에게 맞은 데미지를 계산한다.
+    # t = 2, 포션을 더할 때 최대 체력을 넘지 않도록 주의한다.
+    # 용사가 살아남기 위해 + 1을 한다.
 
+    # !!. 후반부에 포션이 있는 방밖에 없어, hp 계산이 틀릴 수도 있으므로 몬스터를 만날 때마다 필요한 최소 hp를 갱신시켜 주어야 한다.
 
 import sys
 input = sys.stdin.readline
-
-# mid의 체력으로 던전을 통과할 수 있는가?
-def check(mid, attack):
-    initHp = mid
-
-    for [t, a, h] in rooms:
-        if t == 1:
-            monsterAttack = (h - 1) // attack
-            mid -= (monsterAttack * a)
-
-            if mid <= 0:
-                return False
-
-
-        elif t == 2:
-            attack += a
-            mid += h
-            if mid > initHp:
-                mid = initHp
-
-    return True
-
-
-def bns():
-    # check(lo): 0의 체력으로 던전을 통과할 수 없다. | False
-    # check(hi):  hi의 체력으로 던전을 통과할 수 있다. | True
-    lo, hi = 0, N * 10 ** 12
-
-    while lo + 1 < hi:
-        mid = (lo + hi) // 2
-
-        if not check(mid, attack):
-            lo = mid
-
-        else:
-            hi = mid
-
-    return hi
-
 
 # t, a, h
 # t: 1, 공격력: a, 생명력:h 인 몬스터
@@ -62,4 +21,18 @@ def bns():
 N, attack = map(int, input().split())
 rooms = [list(map(int, input().split())) for _ in range(N)]
 
-print(bns())
+result = 0
+hp = 0
+for [t, a, h] in rooms:
+    if t == 1:
+        monsterAttack = (h - 1) // attack
+        hp -= (monsterAttack * a)
+        result = min(result, hp)
+
+    else:
+        attack += a
+        hp += h
+        if hp > 0:
+            hp = 0
+
+print(-result + 1)
