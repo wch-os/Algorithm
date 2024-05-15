f72812ba99f3f32e338419c14f1794c4be2b5586
문선호 <wch1224.s@gmail.com>
2024-05-15 23:54:23 +0900

    [Gold V] Title: 용액, Time: 112 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -3,39 +3,31 @@
 # 공간복잡도 : O(N)
 # 참고 : -
 
-# 출력값 정렬
+def bns():
+    lo, hi = 0, N - 1
+    result = abs(lst[lo] + lst[hi])
+    resultX, resultY = lst[lo], lst[hi]
 
-def lowerBound(tmp):
-    lo, hi = 0, N-1
+    while lo + 1 <= hi:
+        mid = lst[lo] + lst[hi]
 
-    while lo + 1 < hi:
-        mid = (lo + hi) // 2
+        # 두 용액 최솟값 갱신
+        if result >= abs(mid):
+            result = abs(mid)
+            resultX, resultY = lst[lo], lst[hi]
 
-        if tmp > lst[mid]:
-            lo = mid
+        # 투 포인터
+        if mid < 0:
+            lo += 1
         else:
-            hi = mid
+            hi -= 1
 
-    return hi
+    return resultX, resultY
+
 
 
 N = int(input())
 lst = list(map(int, input().split()))
 lst.sort()
 
-result = float('inf')
-for i in range(N):
-    # lowerBound 이므로 abs(lst[i]) 이상 값 인덱스가 나온다.
-    idx = lowerBound(abs(lst[i]))
-
-    # lowerBound 직전 인덱스도 체크 (idx-1, idx)
-    for j in range(idx-1, idx+1):
-        if i == j: # 같은 용액은 배제
-            continue
-
-        num = abs(lst[i] + lst[j])
-        if result >= num:
-            result = num
-            resultX, resultY = lst[i], lst[j]
-
-print(*sorted((resultX, resultY)))
+print(*bns())
