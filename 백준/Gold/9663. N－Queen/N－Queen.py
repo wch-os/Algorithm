6c88836bd0351e0eea0f147c459042e29460f4e6
MoonSunho <wch1224.s@gmail.com>
2024-06-06 16:39:20 +0900

    [Gold IV] Title: N-Queen, Time: 30032 ms, Memory: 202196 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,41 +1,41 @@
-#참고
-#https://seongonion.tistory.com/103
+# 이전 코드
+    # 퀸을 놓을 때 row[x] = y 로 넣음.
 
-# 크기가 N인 체스판 위에, 퀸 N개를 서로 공격할 수 없게 놓는 문제
-N = int(input())
+# 현재 코드
+    # 퀸을 놓을 때 board[x][y] = 1 로 넣음.
+    # 별도의 행열 체크하는 연산이 더 들어간다.
 
-# (i, j)에 퀸을 놓는다면, row[i] = j
-count = 0
-row = [0] * N
-
-def isPromise(x):
-    # 이전 행에 놓여진 퀸들과 비교하기
+def isGood(x):
     for i in range(x):
-        # 같은 열에 다른 퀸이 있는 경우 or 대각선에 다른 퀸이 있는 경우(x축 차이 = y축 차이)
-        if row[x] == row[i] or x - i == abs(row[x] - row[i]):
+        # 같은 열
+        if row[x] == row[i]:
             return False
 
-    # 지금까지 놓여진 퀸의 경로와 겹치지 않는다.
+        # 대각선
+        # 퀸이 놓여 있어야 값이 들어감, 초기값은 0
+        if x - i == abs(row[x] - row[i]):
+            return False
+
     return True
 
 
-def nQueen(start):
-    global count
+result = 0
+def nQueen(curr):
+    global result
 
-    # 종료조건, N개의 퀸을 전부 놓았으면 끝
-    if start == N:
-        count += 1
+    if curr == N:
+        result += 1
         return
 
-    else:
-        for i in range(N):
-            # start 행에 퀸 놓기
-            # (start, i) 지점에 퀸 놓기
-            row[start] = i
+    for j in range(N):
+        row[curr] = j
 
-            # 해당 자리가 유망하면
-            if isPromise(start):
-                nQueen(start+1)
+        if isGood(curr):
+            nQueen(curr + 1)
 
-nQueen(0) # 0번째 행부터 퀸 놓기
-print(count) # 퀸 N개를 서로 공격할 수 없게 놓는 경우의 수
+
+N = int(input())
+row = [0] * N
+
+nQueen(0)
+print(result)
