e301243c2480e7bd54ce90c1a19928c963cd1bd4
MoonSunho <wch1224.s@gmail.com>
2024-06-13 00:46:41 +0900

    [Gold IV] Title: 고층 건물, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1027. 고층 건물/고층 건물.py
+++ 백준/Gold/1027. 고층 건물/고층 건물.py
@@ -1,48 +1,35 @@
-# 풀이 시간: 1시간
-# 시간복잡도: O(N^2)
-# 공간복잡도: O(N)
-# 유형: geometry
-# 참고:
+def solve():
+    result = 0
+    for i in range(N):
+        cnt = 0
 
-# 첫번째 테스트 케이스를 이해하는데 시간이 오래 걸렸다.
-# 직전 값을 이용한 기울기 증감 여부로도 접근할 수 있을 듯 
+        leftSlope = float('inf')
+        rightSlope = -float('inf')
+        for j in range(i-1, -1, -1): # 왼쪽: 기울기가 작아져야 한다.
+            x = i - j
+            y = buildings[i] - buildings[j]
 
-def search(idx):
-    
-    cnt = 0
-    if 0 < idx: # idx = 0일 때 왼쪽 탐색 X
-        cnt += 1 # idx 왼쪽 건물
-        leftSlope = buildings[idx] - buildings[idx - 1]
-        for k in range(idx - 1, -1, -1): # 왼쪽
-            x = idx - k
-            y = buildings[idx] - buildings[k]
-            currSlope = y / x
-
-            if leftSlope > currSlope: # 현재 기울기가 작아져야 한다.
-                leftSlope = currSlope
+            slope = y / x
+            if leftSlope > slope:
+                leftSlope = slope
                 cnt += 1
 
-    if idx < N - 1: # idx = N-1일 때 오른쪽 탐색 X
-        cnt += 1 # idx 오른쪽 건물
-        rightSlope = -(buildings[idx] - buildings[idx + 1])
-        for k in range(idx + 1, N):
-            x = idx - k
-            y = buildings[idx] - buildings[k]
-            currSlope = y / x
 
-            if rightSlope < currSlope: # 현재 기울기가 커져야 한다.
-                rightSlope = currSlope
+        for j in range(i+1, N): # 오른쪽: 기울기가 커져야 한다.
+            x = i - j
+            y = buildings[i] - buildings[j]
+
+            slope = y / x
+            if rightSlope < slope:
+                rightSlope = slope
                 cnt += 1
 
-    return cnt
+        result = max(result, cnt)
 
+    return result
 
 
 N = int(input())
 buildings = list(map(int, input().split()))
 
-result = 0
-for i in range(N):
-    result = max(result, search(i))
-
-print(result)
+print(solve())
