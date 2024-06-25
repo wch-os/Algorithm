d27200a6664ff9d33ca2a8a3ffc02a5507d2ffec
MoonSunho <wch1224.s@gmail.com>
2024-06-25 16:02:15 +0900

    [Gold V] Title: 내려가기, Time: 276 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.py
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -4,7 +4,11 @@
 # 유형: dp
 # 참고: https://velog.io/@hyuntall/%EB%B0%B1%EC%A4%80-2096%EB%B2%88-%EB%82%B4%EB%A0%A4%EA%B0%80%EA%B8%B0-%EB%AC%B8%EC%A0%9C-%ED%92%80%EC%9D%B4-%ED%8C%8C%EC%9D%B4%EC%8D%AC
 
+# 빠른 입력 추가
 # 메모리를 사용을 최소화하기 위한 풀이이다.
+
+import sys
+input = sys.stdin.readline
 
 N = int(input())
 board = list(map(int, input().split()))
