19a6751e8b1be2d76609a4e3307a49d11949b127
MoonSunho <wch1224.s@gmail.com>
2024-08-11 17:52:47 +0900

    [Gold III] Title: 게임 개발, Time: 76 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.py
+++ 백준/Gold/1516. 게임 개발/게임 개발.py
@@ -9,24 +9,24 @@ def solve():
     # 1. 진입 차수가 0인 건물들 세우기
     for i in range(1, N+1):
         if ins[i] == 0:
-            q.append(i)
-            result[i] = costs[i]
+            q.append((i, buildTimes[i]))
+            result[i] = buildTimes[i]
 
     while q:
-        now = q.popleft()
+        now, cost = q.popleft()
 
         for next in board[now]:
             # 2. 해당 건물을 진입차수로 갖는 건물들의 최소 건설 비용 계산하기
                 # next 건물을 짓기 전에 지어야 할 건물들이 여러 개 있을 시
                 # next 건물을 짓기까지의 총 비용 vs. now 건물을 짓기까지의 총 비용 + next 건설 비용
-            result[next] = max(result[next], result[now] + costs[next])
+            result[next] = max(result[next], cost + buildTimes[next])
 
             # 3. 해당 건물을 진입차수로 갖는 건물들의 진입차수 -1
             ins[next] -= 1
 
             # 4. 진입차수가 0인 건물이 나올 시, 해당 건물을 매개로 하는 다른 건물들을 탐색할 수 있도록 queue에 삽입
             if ins[next] == 0:
-                q.append(next)
+                q.append((next, result[next]))
 
 
 
@@ -38,19 +38,19 @@ board = [[] for _ in range(N+1)]
 # 각 건물들의 진입차수 리스트
 ins = [0 for _ in range(N+1)]
 # 각 건물들을 건설하는데 드는 비용 리스트
-costs = [0 for _ in range(N+1)]
+buildTimes = [0 for _ in range(N+1)]
 # 각 건물들을 건설하는데 드는 최소 비용 리스트
 result = [0 for _ in range(N+1)]
 
 for i in range(1, N + 1):
     lst = list(map(int, input().split()))
 
-    costs[i] = lst[0]
+    buildTimes[i] = lst[0]
     for j in range(1, len(lst) - 1):
         # 선행 연관관계 체크
             # i 건물을 짓기 위해 lst[j] 건물을 지어야 함
             # 선수(lst[j]) 노드에 후수(i) 노드를 추가해야 함.
-        board[lst[j]].append(i) 
+        board[lst[j]].append(i)
         ins[i] += 1 # 진입차수 체크
 
 solve()
