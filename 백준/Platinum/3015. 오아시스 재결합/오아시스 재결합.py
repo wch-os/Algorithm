ed4c4c3aa7fb2d25586c9c0d7f4b45fbe698452e
MoonSunho <wch1224.s@gmail.com>
2024-09-21 19:30:36 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 236 ms, Memory: 152384 KB -BaekjoonHub

--- 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.py
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.py
@@ -1,13 +1,4 @@
-# 풀이 시간: 15분(문제이해;;) + 45분(풀이) + 15분(시간초과) + a
-# 시간복잡도: O(N)
-# 공간복잡도: O(N)
-# 유형: stack
-# 참고: https://0902.tistory.com/57
-
-# 1~2번째 풀이
-# - A > B 로 끝날 때, cnt를 적절히 처리하지 못한다.
-# - 반례: 4 5 5 5 6
-
+""" 동일한 키가 있을 경우의 알고리즘 """
 
 import sys
 input = sys.stdin.readline
@@ -16,37 +7,35 @@ N = int(input())
 lst = [int(input()) for _ in range(N)]
 
 stack = []
-result = 0 # 서로 볼 수 있는 쌍의 수
-cnt = 0 # 자신을 포함하여 현재 스택에 같은 값을 가진 사람의 수
+result = 0
 
-for now in lst:
+# num을 기준으로 이전 원소 중 조건에 부합하 원소들만을 stack에서 관리하여, 만족하는 쌍을 찾도록 한다.
+for num in lst:
 
-    # A(now) > B(top)
-    while stack and now > stack[-1][0]:
+    # stack에 num을 기준으로 높은 수만 있다. (내림차순)
+    while stack and stack[-1][0] < num:
         result += stack.pop()[1]
 
-    # 스택이 비어있을 때
-    if not stack:
-        stack.append((now, 1))
-        continue
+
+    if stack:
+        if stack[-1][0] == num:
+            cnt = stack.pop()[1]
+            result += cnt
+
+            # 동일한 원소를 pop() 했음에도, 여전히 stack에 원소가 남아있으면 이는 num보다 큰 수로 바라볼 수 있는 수이다.
+            if stack:
+                result += 1
+
+            stack.append((num, cnt + 1))
 
 
-    # A(now) = B(top)
-    if stack[-1][0] == now:
-
-        cnt = stack.pop()[1]
-        result += cnt
-
-        # B(top)보다 큰 수가 아직 남아있다. ex. 10 1 1 1 5
-        if stack:
+        # stack은 내림차순으로 정렬되어 있으므로, num에서 볼 수 있는 원소는 stack[-1] 밖에 없다.
+        elif stack[-1][0] > num:
             result += 1
+            stack.append((num, 1))
 
-        stack.append((now, cnt + 1))
-
-    # A(now) < B(top)
+    # stack이 비어 있는 경우
     else:
-        result += 1
-        stack.append((now, 1))
-
+        stack.append((num, 1))
 
 print(result)
