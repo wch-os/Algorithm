aadcd069103b87c25f7d5219d0461df53c679fe6
MoonSunho <wch1224.s@gmail.com>
2024-11-07 23:36:45 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 116 ms, Memory: 43864 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.py
@@ -1,44 +1,44 @@
-# 풀이 시간: 25분
+# 풀이 시간: 25분 + 1시간
 # 시간복잡도: O(3*(N-K))
 # 공간복잡도: O(3*(N-K))
 # 유형: bfs
 # 참고: -
 
-# x에서 y까지 가는 총 방법의 수를 카운트해야 하므로, visited는 사용하지 않음
+# 가장 빠르게 이동하는 노드들을 큐에 넣어서 "x = y" 일 경우를 모두 찾는 방법과
+# 가장 빠르게 이동하면서 중복 방문하는 횟수를 따로 저장해서 "x = y"일 때 바로 return 되는 두 가지 방법으로 풀 수 있을 듯하다.
 
 from collections import deque
 
 def bfs(x):
-    # i 지점까지 가는 [가장 빠른 시간, 방법 수]
+    # i 지점까지 가는 [가장 빠른 시간, 방법]
     visited = [[0, 0] for _ in range(100001)]
     visited[x] = [0, 1]
 
-    minMove = 100000
-    totalCnt = 0
-
     q = deque()
-    q.append((x, 0))
+    q.append(x)
     while q:
-        x, cnt = q.popleft()
+        x = q.popleft()
 
         if x == y:
-            minMove = cnt
-            totalCnt = visited[x][1]  # 최소 이동 횟수로, 목적지까지의 루트 개수
             break
 
-        for k in (x - 1, x + 1, x * 2):
+        for nx in (x - 1, x + 1, x * 2):
             # x가 범위값을 벗어나는 경우
-            if k < 0 or k > 100000:
+            if nx < 0 or nx > 100000:
                 continue
 
-            if not visited[k][0]:  # 첫 방문일 경우
-                visited[k] = [cnt + 1, visited[x][1]]
-                q.append((k, cnt + 1))
-            else: # 중복 방문이면서
-                if visited[k][0] == cnt + 1:  # 동일한 최소 이동 횟수라면, 별도로 카운트 함
-                    visited[k][1] += visited[x][1]
+            # 첫 방문일 때, 시간 및 방법 추가
+            if not visited[nx][0]:
+                visited[nx][0] = visited[x][0] + 1
+                visited[nx][1] += visited[x][1]
+                q.append(nx)
 
-    return minMove, totalCnt
+            # 최소 이동으로 중복 방문일 경우, 방법 추가
+            elif visited[nx][0] == visited[x][0] + 1:
+                visited[nx][1] += visited[x][1]
+
+
+    return visited[x][0], visited[x][1]
 
 
 x, y = map(int, input().split())
